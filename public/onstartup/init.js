20d72dfd1fd0ff0e4ca5fd2195c9454ec87c2950
theskid22 <shrey150436@gmail.com>
2024-12-14 20:35:20 -0500

    appsearch

--- public/onstartup/init.js
+++ public/onstartup/init.js
@@ -5,17 +5,34 @@ this.sysPopupOn = false;
 
 // Window manager
 document.addEventListener('keydown', function(event) {
-    if (event.altKey && event.key === 'd') {
+    if (event.altKey && event.key === 's') {
         if (sysPopupOn) {
+            event.preventDefault()
             return;
         }
         event.preventDefault()
         let elm = document.createElement("div")
-        elm.style.width = "20%"
-        elm.style.height = "20%"
+        let appSearch = document.createElement("input")
+        elm.style.width = "30%"
+        elm.style.border = "0.2rem solid blue";
+        elm.style.borderRadius = "0.5rem";
+        elm.style.height = "25%"
+
         elm.classList.add("popup")
-        elm.style.backgroundColor = "blue"
+
+        appSearch.style.border = "0.2rem solid black"
+        appSearch.style.padding = "0.5rem"
+        appSearch.style.color = "black"
+        appSearch.style.borderRadius = "0.5rem"
+        appSearch.style.width = "90%"
+        
+        appSearch.style.marginTop = "5%";
+        appSearch.style.marginLeft = "5%";
+        appSearch.style.marginRight = "5%";
+        appSearch.placeholder = "Search for an app..."
+        elm.appendChild(appSearch)
         document.body.appendChild(elm)
+        appSearch.focus()
         sysPopupOn = true;
     }
     if (event.key === 'Escape') {
